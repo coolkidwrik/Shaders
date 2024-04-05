b88b33de974da794c3b93f826843c16127b1d46b
Wrik <coolkidwrik@gmail.com>
2024-04-05 11:41:30 -0700

    added images of orbs

--- mirror/mirror.fs.glsl
+++ mirror/mirror.fs.glsl
@@ -1,6 +1,7 @@
 // https://makc3d.wordpress.com/2017/01/19/sampling-equirectangular-textures/
 in vec3 vcsNormal;
 in vec3 vcsPosition;
+in vec2 vUv;
 
 uniform samplerCube skybox;
 
@@ -11,6 +12,7 @@ void main( void ) {
   vec3 reflectionVector = reflect(normalize(vcsPosition), normalize(vcsNormal));
 
   gl_FragColor = texture(skybox, vec3(matrixWorld*vec4(reflectionVector, 0.0)));
+  // gl_FragColor = vec4(texture(skybox, vUv).xyz, 1.0);
 
   // gl_FragColor = texture(skybox, vec3(inverse(viewMatrix)*vec4(reflectionVector, 0.0))); // alternative
 }
--- mirror/mirror.vs.glsl
+++ mirror/mirror.vs.glsl
@@ -1,11 +1,13 @@
 out vec3 vcsNormal;
 out vec3 vcsPosition;
+// out vec2 vUv;
 
 
 void main() {
 	// viewing coordinate system
 	vcsNormal = normalMatrix * normal; 
 	vcsPosition = vec3(modelViewMatrix * vec4(position, 1.0));
+	// vUv = uv;
     
 	gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
 }
